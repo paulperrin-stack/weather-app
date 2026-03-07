bdc980048fb90d3c1c8c1f675f38a3c4df6b41f5
Paul <247144100+paulperrin-stack@users.noreply.github.com>
2026-03-07 10:35:52 +0700

    chore: set up webpack with npm and dev dependencies

---
+++ webpack.config.js
@@ -0,0 +1,47 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+    mode: "development", // change to "production" for final build
+
+    entry: "./src/index.js",
+
+    output: {
+        filename: "main.js",
+        path: path.resolve(__dirname, "dist"),
+        clean: true,
+    },
+
+    devServer: {
+        static: "./dist",
+        port: 3000,
+        open: true,
+        hot: true,
+    },
+
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "./src/index.html", // uses your HTML as the base
+        }),
+    ],
+
+    module: {
+        rules: [
+            // CSS support
+            {
+                test: /\.css$/i,
+                use: ["style-loader", "css-loader"],
+            },
+            // Image support (no extra loader needed in Webpack 5)
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: "asset/resource",
+            },
+            // Fonts support
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)&/i,
+                type: "asset/resource"
+            },
+        ],
+    },
+};
