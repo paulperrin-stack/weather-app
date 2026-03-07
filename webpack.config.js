472f746c62a77dc1177bcb46044e0b8a5ccad065
Paul <247144100+paulperrin-stack@users.noreply.github.com>
2026-03-07 15:26:44 +0700

    chore: add gh-pages deployment

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,7 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const webpack = require("webpack");
+const dotenv = require("dotenv")
 
 module.exports = {
     mode: "production",
